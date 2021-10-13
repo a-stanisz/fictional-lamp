f310a43ff570b5f49bc6f952aa66bdda46a4a799
a-stanisz <hello@adamstaniszewski.com>
2021-10-13 13:37:46 +0200

    Add storing posts in the db

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,20 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
         _id: '1',
-        title: 'Lorem',
+        title: 'Lorem ipsum',
         content: 'Ipsum dolor',
-        imageUrl: 'images/duck.png',
+        imageUrl: 'images/quantum-cat.jpg',
         creator: {
           name: 'Adam',
         },
-        createdAt: new Date()
+        createdAt: new Date(),
       },
     ],
   });
@@ -23,23 +25,31 @@ exports.postPost = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(422).json({
       message: 'Validation error',
-      errors: errors.array()
+      errors: errors.array(),
     });
   }
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title, content);
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created!',
-    post: {
-      _id: uuidv4(),
-      title: title,
-      content: content,
-      creator: {
-        name: 'Adam',
-      },
-      createdAt: new Date()
+  const post = new Post({
+    title: title,
+    imageUrl: 'images/quantum-cat.jpg',
+    content: content,
+    creator: {
+      name: 'Adam',
     },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created!',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log(title, content);
+  // Create post in db
 };
