78803ec28cf2954bf86c95dca644e6a2f55bb5bf
a-stanisz <hello@adamstaniszewski.com>
2021-11-04 15:09:13 +0100

    Add pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,30 +7,30 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
-      res.status(200).json({ message: 'Posts fetched!', posts: posts })
+      res.status(200).json({
+        message: 'Posts fetched!',
+        posts: posts,
+        totalItems: totalItems
+      })
     })
     .catch(err => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
-    });
-  // res.status(200).json({
-  //   posts: [
-  //     {
-  //       _id: '1',
-  //       title: 'Lorem ipsum',
-  //       content: 'Ipsum dolor',
-  //       imageUrl: 'images/quantum-cat.jpg',
-  //       creator: {
-  //         name: 'Adam',
-  //       },
-  //       createdAt: new Date(),
-  //     },
-  //   ],
-  // });
+    })
 };
 
 exports.postPost = (req, res, next) => {
@@ -128,6 +128,31 @@ exports.updatePost = (req, res, next) => {
     })
     .then(result => {
       res.status(200).json({ message: 'Post updated!', post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post!');
+        error.statusCode = 404;
+        throw error;
+      }
+      // is user logged?
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Post deleted!' });
     })
     .catch(err => {
       if (!err.statusCode) {
